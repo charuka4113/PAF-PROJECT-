3a00b87b5b78ca7fc977b1aee55fb727dffeb47e
janith <it19126616@my.sliit.lk>
2021-04-22 11:45:38 +0530

    2.0 get,post and put

---
+++ src/Controller/PaymentController.java
@@ -0,0 +1,159 @@
+package Controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+
+public class PaymentController {
+	
+	private Connection connect() 
+	{ 
+		Connection con = null; 
+		try
+		{ 
+			Class.forName("com.mysql.jdbc.Driver"); 
+ 
+			//Provide the correct details: DBServer/DBName, username, password 
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/pafprojtest?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root",""); 
+		} 
+		catch (Exception e) 
+		{e.printStackTrace();} 
+		return con; 
+	} 
+	
+	public String readPayment() 
+	{ 
+		 String output = ""; 
+		 
+		 try
+		 { 
+			 Connection con = connect(); 
+		 
+			 if (con == null) 
+			 {return "Error while connecting to the database for reading."; } 
+		 
+			 // Prepare the html table to be displayed
+			 output = "<table border='1'><tr><th>Pay ID</th><th>Pay Date</th><th>Description</th><th>Price</th><th>Type</th>"; 
+		 
+			 String query = "select * from payment"; 
+			 Statement stmt = con.createStatement(); 
+			 ResultSet rs = stmt.executeQuery(query); 
+		 
+			 // iterate through the rows in the result set
+			 while (rs.next()) 
+			 { 
+				 int payid = rs.getInt("payid");
+				 String paydate = rs.getString("paydate");
+				 String description = rs.getString("description"); 
+				 int price = rs.getInt("price");
+				 String type = rs.getString("type");
+				 
+				 // Add into the html table
+				 output += "<tr><td>" + payid + "</td>"; 
+				 output += "<td>" + paydate + "</td>"; 
+				 output += "<td>" + description + "</td>"; 
+				 output += "<td>" + price + "</td>"; 
+				 output += "<td>" + type + "</td>"; 
+				  
+				 // buttons
+				 output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"
+						+ "<td><input name='btnDelete' type='submit' value='Delete' class='btn btn-danger'></td>"
+						+ "<input name='payid' type='hidden' value='" + payid 
+						+ "'>" + "</form></td></tr>"; 
+		 } 
+		 
+		con.close(); 
+		 
+		// Complete the html table
+		 output += "</table>"; 
+	} 
+	catch (Exception e) 
+	{ 
+		 output = "Error while reading the items."; 
+		 System.err.println(e.getMessage()); 
+	} 
+		 return output; 
+} 
+
+	
+	public String InsertPayment(String paydate, String description, int price, String type) 
+	{ 
+		String output = ""; 
+	
+		try
+		{ 
+			Connection con = connect(); 
+			if (con == null) 
+			{return "Error while connecting to the database for inserting."; } 
+ 
+			// create a prepared statement
+			String query = " insert into pafprojtest.payment (`payid`,`paydate`,`description`,`price`,`type`)"
+					+ " values (?,?,?,?,?)"; 
+ 
+			PreparedStatement preparedStmt = con.prepareStatement(query); 
+
+			// binding values
+			
+			preparedStmt.setInt(1, 0); 
+			preparedStmt.setString(2, paydate); 
+			preparedStmt.setString(3, description); 
+			preparedStmt.setInt(4, price);
+			preparedStmt.setString(5, type);
+			
+			preparedStmt.execute(); 
+			 con.close(); 
+			 
+			 output = "Inserted successfully"; 
+		} 
+		catch (Exception e) 
+		{ 
+			 output = "Error while inserting the item."; 
+			 System.err.println(e.getMessage()); 
+		} 
+			 
+		return output; 
+	}
+	
+
+	public String updatePayment(String payid,String paydate, String description, String price, String type)
+	{ 
+		 String output = ""; 
+		 try
+		 { 
+			 	Connection con = connect(); 
+		 
+			 	if (con == null) 
+			 	{return "Error while connecting to the database for updating."; } 
+		 
+			 	// create a prepared statement
+			 	String query = "UPDATE payment SET paydate=?,description=?,price=?,type=? WHERE payid =?"; 
+		 
+			 	PreparedStatement preparedStmt = con.prepareStatement(query); 
+		 
+			 	// binding values
+			 	preparedStmt.setString(1, paydate); 
+			 	preparedStmt.setString(2, description); 
+			 	preparedStmt.setInt(3, Integer.parseInt(price)); 
+			 	preparedStmt.setString(4, type); 
+			 	
+			 	preparedStmt.setInt(5, Integer.parseInt(payid)); 
+			
+			 	// execute the statement
+			 	preparedStmt.execute(); 
+			 	con.close(); 
+			 	output = "Updated successfully"; 
+		 } 
+		 catch (Exception e) 
+		 { 
+			 	output = "Error while updating the item."; 
+			 	System.err.println(e.getMessage()); 
+		 } 
+		 
+		 return output; 
+		 
+	}
+}
